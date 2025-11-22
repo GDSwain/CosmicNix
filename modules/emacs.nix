3affa87320afa6b51b052689c5d989dec3a3133f
GDSwain <guy@houseofswain.com>
2025-11-22 15:20:59 -0800

    Auto backup: 2025-11-22T15:20:58-08:00

--- modules/emacs.nix
+++ modules/emacs.nix
@@ -4,7 +4,7 @@
   services.emacs = {
     enable = true;              # turn on the user emacs daemon
     install = true;             # install the user service
-    package = pkgs.emacs;       # use your chosen Emacs build
+    package = pkgs.emacs-pgtk;  # use your chosen Emacs build
     startWithGraphical = true;  # start when your graphical session starts
     defaultEditor = true;       # optional: sets EDITOR=emacsclient
   };
